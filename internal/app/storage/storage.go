8347efd06c7fc35e3e75ce7da154e66b68d910f4
yten <ten.djenia@gmail.com>
2024-11-25 11:00:44 +0500

    + fix build

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -12,12 +12,23 @@ func NewShortURLStorage() *ShortURLStorage {
 	}
 }
 
+//func NewShortURLStorage() ShortURLStorage {
+//	return ShortURLStorage{
+//		storage: make(map[string]string),
+//	}
+//}
+
 func (storage *ShortURLStorage) Save(url string) string {
 	newID := generateRandomId.GenerateShortID()
 	storage.storage[newID] = url
 	return newID
 }
 
+//	func (storage *ShortURLStorage) SaveTest(url string) *ShortURLStorage {
+//		newID := generateRandomId.GenerateShortID()
+//		storage.storage[newID] = url
+//		return storage
+//	}
 func (storage *ShortURLStorage) GetURL(id string) string {
 	return storage.storage[id]
 }
