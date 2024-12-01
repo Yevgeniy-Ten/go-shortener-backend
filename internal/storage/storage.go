69f93d6783a5ed0f2ee090ab685ca456cbf3fa60
yten <ten.djenia@gmail.com>
2024-12-01 19:25:23 +0500

    +

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -2,21 +2,26 @@ package storage
 
 import (
 	generateRandomId "shorter/pkg"
+	"sync"
 )
 
 type ShortURLStorage struct {
 	storage map[string]string
+	mutex   *sync.Mutex
 }
 
 func NewShortURLStorage() *ShortURLStorage {
 	return &ShortURLStorage{
 		storage: make(map[string]string),
+		mutex:   &sync.Mutex{},
 	}
 }
 
 func (storage *ShortURLStorage) Save(url string) string {
 	newID := generateRandomId.GenerateShortID()
+	storage.mutex.Lock()
 	storage.storage[newID] = url
+	defer storage.mutex.Unlock()
 	return newID
 }
 
