256d993e99fd42e3e5cf6d2a9c5393b3753db916
yten <ten.djenia@gmail.com>
2025-01-22 09:37:29 +0500

    iter14

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -51,7 +51,7 @@ func TestUrlHandlers(t *testing.T) {
 		},
 		nil,
 	)
-	r := h.CreateRouter(gzipper.RequestResponseGzipMiddleware())
+	r := h.CreateRouter(false, gzipper.RequestResponseGzipMiddleware())
 
 	createURL := func(value, contentType, url string) *http.Response {
 		request := httptest.NewRequest("POST", url, strings.NewReader(value))
